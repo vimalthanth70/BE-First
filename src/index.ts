c851aad77765c5846b4997e2586d1472ee88db78
unknown <vimalthanth70@gmail.com>
2024-03-30 09:32:54 +0530

    Changed poprt

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ app.get("/",(req,res)=>{
     res.send("This is vimal's website")
 })
 
-app.listen(443,()=>{
-    console.log('server is running on port 443')
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
 })
